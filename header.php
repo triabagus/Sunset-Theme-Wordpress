035abcd4902954071c19fc46f195bea71ca1ef38
triabagus <triatop9@gmail.com>
2019-11-03 10:18:20 +0700

    create dropdown menu and styling navbar and change version bootstrap 4 JS add popper js

--- header.php
+++ header.php
@@ -47,7 +47,11 @@
                                 wp_nav_menu( array(
                                     'theme_location'    => 'primary',
                                     'container'         => false,
-                                    'menu_class'        => 'nav nav-item'
+                                    'menu_class'        => 'nav nav-item',
+                                    'fallback_cb'    => '__return_false',
+                                    'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                                    'depth'          => 2,
+                                    'walker'            => new Sunset_Walker_Nav_Primary()
                                 ) );
                             ?>
                         </nav>
--- inc/enqueue.php
+++ inc/enqueue.php
@@ -43,13 +43,14 @@ add_action('admin_enqueue_scripts', 'sunset_load_admin_scripts');
 */
 function sunset_load_scripts()
 {
-    wp_enqueue_style('bootstrap', get_template_directory_uri(). '/css/bootstrap.min.css', array(), '4.3.1', 'all');
     wp_enqueue_style('sunset', get_template_directory_uri(). '/css/sunset.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('bootstrap', get_template_directory_uri(). '/css/bootstrap.min.css', array(), '4.3.1', 'all');
     wp_enqueue_style('raleway', 'https://fonts.googleapis.com/css?family=Raleway:200,300,400,500&display=swap');
 
     wp_deregister_script('jquery');
-    wp_register_script('jquery', get_template_directory_uri(). '/js/jquery.js', false, '1.11.3', true);
+    wp_register_script('jquery', get_template_directory_uri(). '/js/jquery.js', false, '3.3.1', true);
     wp_enqueue_script('jquery');
+    wp_enqueue_script('popper', get_template_directory_uri(). '/js/popper.js', array('jquery'), '1.14.7', true);
     wp_enqueue_script('bootstrap', get_template_directory_uri(). '/js/bootstrap.min.js', array('jquery'), '4.3.1', true);
 }
 add_action('wp_enqueue_scripts', 'sunset_load_scripts');
