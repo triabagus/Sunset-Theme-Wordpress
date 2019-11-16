cd54841b9a3d7b1fef158e7e65acda4e4d7f0ea4
triabagus <triatop9@gmail.com>
2019-11-16 10:04:34 +0700

    Create comment section page count comment

--- comments.php
+++ comments.php
@@ -14,6 +14,39 @@ if( post_password_required() ){
         if( have_comments() ):
             //we have comments
     ?>
+
+    <h2 class="comment-title">
+        <?php
+            printf(
+                esc_html( _nx('One comments on &ldquo;%2$s&rdquo;', '%1$s comments on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'sunsettheme') ),
+                number_format_i18n( get_comments_number() ),
+                '<span>'.get_the_title().'</span>' 
+            );
+        ?>
+    </h2>
+
+    <?php
+        if( get_comment_pages_count() > 1 && get_option( 'page_comments') ): 
+    ?>
+        <nav id="comment-nav-top" class="comment-navigation" role="navigation">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6">
+                    <div class="post-link-nav">
+                        <span class="sunset-icon sunset-chevron-left" aria-hidden="true"></span>
+                        <?php previous_comments_link( esc_html__( 'Older Comments', 'sunsettheme'))?>
+                    </div>
+                </div>
+                
+                <div class="col-xs-12 col-sm-6 text-right">
+                    <div class="post-link-nav">
+                        <span class="sunset-icon sunset-chevron-right" aria-hidden="true"></span>
+                        <?php next_comments_link( esc_html__( 'Newer Comments', 'sunsettheme'))?>
+                    </div>
+                </div>
+            </div><!-- .row -->
+        </nav>
+    <?php endif; ?>
+    
     <ol class="comment-list">
 
         <?php
@@ -27,7 +60,7 @@ if( post_password_required() ){
                 'reply_text'        => 'Reply',
                 'page'              => '',
                 'per_page'          => '',
-                'avatar_size'       => 32,
+                'avatar_size'       => 64,
                 'reverse_top_level' => null,
                 'reverse_children'  => '',
                 'format'            => 'html5',
@@ -39,7 +72,29 @@ if( post_password_required() ){
         ?>
     
     </ol>
-        
+    
+    <?php
+        if( get_comment_pages_count() > 1 && get_option( 'page_comments') ): 
+    ?>
+        <nav id="comment-nav-top" class="comment-navigation" role="navigation">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6">
+                    <div class="post-link-nav">
+                        <span class="sunset-icon sunset-chevron-left" aria-hidden="true"></span>
+                        <?php previous_comments_link( esc_html__( 'Older Comments', 'sunsettheme'))?>
+                    </div>
+                </div>
+                
+                <div class="col-xs-12 col-sm-6 text-right">
+                    <div class="post-link-nav">
+                        <span class="sunset-icon sunset-chevron-right" aria-hidden="true"></span>
+                        <?php next_comments_link( esc_html__( 'Newer Comments', 'sunsettheme'))?>
+                    </div>
+                </div>
+            </div><!-- .row -->
+        </nav>
+    <?php endif; ?>
+
         <?php if( !comments_open() && get_comments_number() ): ?>
             <p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'sunsettheme'); ?></p>
         <?php endif;?>
