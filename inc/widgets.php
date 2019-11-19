563aadd32a84c6e49d03886a009358f4c6f9081e
triabagus <triatop9@gmail.com>
2019-11-19 10:11:13 +0700

    Popular post format widget done in back end and front end update

--- inc/widgets.php
+++ inc/widgets.php
@@ -100,3 +100,84 @@ function sunset_save_post_views( $postID)
 }
 
 remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+
+
+/*
+	Popular Posts Widget
+*/
+
+class Sunset_Popular_Posts_Widget extends WP_Widget
+{
+	//Setup the widget name , description and etc ...
+	public function __construct()
+	{
+		$widget_ops = array(
+			'classname'		=> 'sunset-popular-posts-widget',
+			'description'	=> 'Popular Posts Widget',
+		);
+
+		parent::__construct( 'sunset_popular_posts', 'Sunset Popular Posts', $widget_ops);
+	}
+
+	//back-end display of widget
+	public function form( $instance)
+	{
+		$title		 = ( !empty( $instance['title'] ) ? $instance['title'] : "Popular Posts");
+		$sum		 = ( !empty( $instance['sum'] ) ? absint( $instance['sum'] ) : 4 );
+
+		$output		 = '<p>';
+		$output		.= '<label for="'.esc_attr( $this->get_field_id('title') ).'">Title :</label>';
+		$output		.= '<input type="text" class="widefat" id="'.esc_attr( $this->get_field_id('title')).'" name="'.esc_attr( $this->get_field_name('title') ).'" value="'.esc_attr($title).'">';
+		$output		.= '</p>';
+		
+		$output		.= '<label for="'.esc_attr( $this->get_field_id('sum') ).'">Number of Posts :</label>';
+		$output		.= '<input type="number" class="widefat" id="'.esc_attr( $this->get_field_id('sum')).'" name="'.esc_attr( $this->get_field_name('sum') ).'" value="'.esc_attr($sum).'">';
+		$output		.= '</p>';
+
+		echo $output;
+	}
+
+	//update widget
+	public function update( $new_instance, $old_instance)
+	{
+		$instance			= array();
+		$instance['title']	= ( !empty($new_instance['title']) ? strip_tags( $new_instance['title']): '');
+		$instance['sum']	= ( !empty($new_instance['sum']) ? absint(strip_tags( $new_instance['sum'])): 0);
+
+		return $instance;
+	}
+
+	//front-end of widget
+	public function widget( $args, $instance)
+	{
+		$sum		= absint( $instance['sum']);
+
+		$posts_args = array(
+			'post_type'			=> 'post',
+			'posts_per_page'	=> $sum,
+			'meta_key'			=> 'sunset_post_views',
+			'orderby'			=> 'meta_value_num',
+			'order'				=> 'DESC'
+		);
+
+		$posts_query			= new WP_Query( $posts_args);
+
+		echo $args['before_widget'];
+			if( !empty( $instance['title'] ) ):
+				echo $args['before_title'].apply_filters('widget_title', $instance['title']). $args['after_title'];
+			endif;
+
+			if( $posts_query->have_posts()):
+				echo '<ul>';
+					while($posts_query->have_posts()) : $posts_query->the_post();
+						echo '<li>'.get_the_title().'</li>';
+					endwhile;
+				echo '</ul>';
+			endif;
+		echo $args['after_widget'];
+	}
+}
+
+add_action( 'widgets_init', function(){
+	register_widget( 'Sunset_Popular_Posts_Widget');
+});
