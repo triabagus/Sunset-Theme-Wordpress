b8673039ecdc2a9dd04d2fd31e95a624a3e8d529
triabagus <triatop9@gmail.com>
2019-11-20 13:33:09 +0700

    Trial for SMTP Local Mailtrap send message in Contact Form

--- inc/theme-support.php
+++ inc/theme-support.php
@@ -249,3 +249,19 @@ function sunset_get_post_navigation()
         require( get_template_directory(). '/inc/templates/sunset-comment-nav.php');
     // endif;
 }
+
+/*
+	========================
+	TRIAL MAILTRAP SMTP LOCAL FUNCTIONS
+	========================
+*/
+function mailtrap($phpmailer) {
+    $phpmailer->isSMTP();
+    $phpmailer->Host = 'smtp.mailtrap.io';
+    $phpmailer->SMTPAuth = true;
+    $phpmailer->Port = 2525;
+    $phpmailer->Username = '';
+    $phpmailer->Password = '';
+}
+
+add_action('phpmailer_init', 'mailtrap');
