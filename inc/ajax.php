676ee4cc5c10cdff9193b8c06a5237e6e752e92b
triabagus <triatop9@gmail.com>
2019-11-20 13:20:29 +0700

    Create default send Email SMTP

--- inc/ajax.php
+++ inc/ajax.php
@@ -129,6 +129,17 @@ function sunset_save_contact()
 
     $postID = wp_insert_post($args);
 
+    if($postID !== 0){
+        $to         = get_bloginfo('admin_email');
+        $subject    = 'Sunset Contact Form - '. $title;
+        
+        $headers[]  = 'From: '.get_bloginfo('name').'<'.$to.'>'; //From: Tria <triabagus@gmail.com>
+        $headers[]  = 'Reply-To: '.$title.'<'.$email.'>';
+        $headers[]  = 'Content-Type: text/html: charset=UTF-8';
+        
+        wp_mail($to, $subject, $message, $headers);
+    }
+
     echo $postID;
     // echo 0;
 
